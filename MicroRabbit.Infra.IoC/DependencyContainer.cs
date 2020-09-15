62ac701b9083aede8c816df10436f69ac53e5d57
Sandeep <sandeep.28878@gmail.com>
2020-09-15 15:33:14 +0530

    Intial Commit for Infrastruture Dependency

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Infrastructure.bus;
+using Microsoft.Extensions.DependencyInjection;
+using MircroRabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            // DomainBus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
